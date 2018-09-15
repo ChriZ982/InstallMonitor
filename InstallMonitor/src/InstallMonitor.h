48ba499307bcd44a613045ec0f93727aecefc555
ChriZ982 <christopher.zinda2@googlemail.com>
2018-09-15 17:02:31 +0200

    Scanning the filesystem and comparing two scans is working.

---
+++ InstallMonitor/src/InstallMonitor.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <vector>
+
+std::vector<std::string> list_directories(std::string path);
+void scan_filesystem();
+void compare_filesystem();
+bool is_excluded(std::string, std::vector<std::string> excludes);
