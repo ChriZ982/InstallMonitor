6226f3c565ba734062fb4b5beb2e77a4c8a978b1
ChriZ982 <christopher.zinda2@googlemail.com>
2018-11-11 17:29:14 +0100

    Supported multiple architectures

--- build.bat
+++ build.bat
@@ -1,10 +1,25 @@
 @echo off
-call "C:/Program Files (x86)/Microsoft Visual Studio/2017/BuildTools/VC/Auxiliary/Build/vcvarsall.bat" x64
+if defined TRAVIS_TAG ( set "version=%TRAVIS_TAG%" ) else ( set "version=local" )
+set build_tools="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvarsall.bat"
 
-set out_dir=build
-if defined TRAVIS_TAG ( set version=%TRAVIS_TAG% ) else ( set version=local )
-set compilerflags=/GL /Gy /Zi /Gm- /Ox /sdl /Fd"%out_dir%/vc141.pdb" /Zc:inline /Oi /MD /EHsc /nologo /Fo"%out_dir%/"
-set linkerflags=/OUT:"%out_dir%/InstallMonitor-%version%-win32-x64.exe" /PDB:"%out_dir%/InstallMonitor.pdb" /DEBUG /OPT:REF /OPT:ICF 
+set out_dir=build\debug
+set compilerflags=/GL /Gy /Zi /Gm- /sdl /Fd"%out_dir%\vc141.pdb" /Zc:inline /MD /EHsc /nologo /Fo"%out_dir%/"
+set linkerflags=/PDB:"%out_dir%/InstallMonitor.pdb" /DEBUG /OPT:REF /OPT:ICF
+md %out_dir%
 
-mkdir %out_dir%
-cl.exe %compilerflags% src/*.cpp /link %linkerflags%
+call %build_tools% x64
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
+
+call %build_tools% x86
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
+
+set out_dir=build\release
+set compilerflags=/GL /Gy /Gm- /Ox /sdl /Zc:inline /Oi /MD /EHsc /nologo /Fo"%out_dir%/"
+set linkerflags=/OPT:REF /OPT:ICF
+md %out_dir%
+
+call %build_tools% x64
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
+
+call %build_tools% x86
+cl.exe %compilerflags% src\*.cpp /link %linkerflags% /OUT:"%out_dir%\InstallMonitor-%version%-win32-%VSCMD_ARG_TGT_ARCH%.exe"
--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 out_dir="build"
 version=${TRAVIS_TAG:-"local"}
-compilerflags="-std=c++17 -Wall -Wextra -Ofast"
+compilerflags="-std=c++17 -Wall -Wextra"
 linkerflags="-lstdc++fs"
 
-mkdir $out_dir
-clang++ $compilerflags src/*.cpp -o $out_dir/InstallMonitor-$version-linux-x64 $linkerflags
+mkdir -p ${out_dir}/release ${out_dir}/debug
+clang++ ${compilerflags} -target x86_64-linux src/*.cpp -o ${out_dir}/debug/InstallMonitor-${version}-linux-x64 ${linkerflags}
+clang++ ${compilerflags} -target i386-linux src/*.cpp -o ${out_dir}/debug/InstallMonitor-${version}-linux-x86 ${linkerflags}
+clang++ ${compilerflags} -Ofast -target x86_64-linux src/*.cpp -o ${out_dir}/release/InstallMonitor-${version}-linux-x64 ${linkerflags}
+clang++ ${compilerflags} -Ofast -target i386-linux src/*.cpp -o ${out_dir}/release/InstallMonitor-${version}-linux-x86 ${linkerflags}
